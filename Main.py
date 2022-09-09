17917313b673bb86b785d615b140153b0b76ed7a
URK21CS1073 <109951867+URK21CS1073@users.noreply.github.com>
2022-09-09 22:23:06 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,48 +1,52 @@
-class node:
-    def __init__(s,x):
-        s.data=x
-        s.next=None
-class stackList:
-    def __init__(self):
-        self.top=None
-    def push(self,x):
-        n=node(x)
-        n.next=self.top
-        self.top=n
-    def displayIO(self):
-        self.displayInorder1(self.top)
-    def displayInorder1(self,t):
-        if t!=None:
-            self.displayInorder1(t.next)
-            print(t.data)
-    def display(self):
-        if self.top==None:
-            print("Stack has no element")
-        else:
-            temp=self.top
-            while temp!=None:
-                print(temp.data)
-                temp=temp.next
-    def pop(self):
-        if self.top==None:
-            print("Stack has no element")
-        else:
-            pops = self.top
-            self.top = self.top.next
-            pops.next = None
-            return pops.data
-sul=stackList()
-while(1):
-    print("1.Push,\n2.pop,\n3.Display In_order,\n4.Display in last in first out order,\n5. Exit")
-    h=int(input("enter your choice to be done:"))
-    if h==1:
-        x=int(input("Enter the data to be pushed:"))
-        sul.push(x)
-    elif h==2:
-        sul.pop()
-    elif h==3:
-        sul.displayIO()
-    elif h==4:
-        sul.display()
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
+
+class Stack:
+  def __init__(self):
+    self.head = None
+
+  def push(self, data) -> None:
+    # Write your code here
+    new_node = Node(data)
+    new_node.next = self.head
+    self.head = new_node
+
+  def pop(self) -> None:
+    # Write your code here
+    if(self.head != None): 
+      temp = self.head
+      self.head = temp.next
+      temp.next = None
+
+  def status(self):
+    """
+    It prints all the elements of stack.
+    """
+    # Write your code here 
+    if(self.head != None):
+      temp = self.head
+      while(temp.next != None):
+        print(temp.data, end = "=>")
+        temp = temp.next
+      else:
+        print(temp.data,"None", sep="=>")
     else:
-        break
+      print("None")
+
+
+# Do not change the following code
+stack = Stack()
+operations = []
+for specific_operation in input().split(','):
+    operations.append(specific_operation.strip())
+input_data = input()
+data = input_data.split(',')
+for i in range(len(operations)):
+  if operations[i] == "push":
+    stack.push(int(data[i]))
+  elif operations[i] == "pop":
+    stack.pop()
+stack.status()
